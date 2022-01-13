9d7d01ce37dd5ebda5c39a1d315abed5f312eef1
MIN-JEA-KIM <blackbread@nate.com>
2022-01-13 14:13:15 +0900

    su

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
 from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("news/", include("news.urls")),
     path("", RedirectView.as_view(url="/news/", permanent=True)),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- news/urls.py
+++ news/urls.py
@@ -11,6 +11,5 @@ urlpatterns = [
     path("art/", views.art, name="art"),
     path("world/", views.world, name="world"),
     path("travel/", views.travel, name="travel"),
-    #path("travel1/", views.travel1, name="travel1"),
     path("travel1/", views.NCategory, name="NCategory"),
 ]
