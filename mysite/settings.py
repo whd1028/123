ee1725cfcf9ba2356cec2171959b38d79d154381
ìsangwoo1247 <sangwoo1247@gmail.com>
2022-01-18 18:16:49 +0900

    mp3 add

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'news.apps.NewsConfig',
+    'debug_toolbar',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- news/views.py
+++ news/views.py
@@ -1,5 +1,9 @@
+import os
 from django.shortcuts import render
 from .models import *
+# from rest_framework.response import Response
+# from rest_framework import status
+# from rest_framework.decorators import api_view
 
 
 # Create your views here.
