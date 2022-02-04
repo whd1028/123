5b99c1c59038c78aad771340783169c2c1eee889
MIN-JEA-KIM <blackbread@nate.com>
2022-02-04 19:47:23 +0900

    0204

--- news/views.py
+++ news/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import *
 
 # -2022.01.24 park_jong_won
@@ -8,42 +9,25 @@ logger = logging.getLogger('news')
 
 # Create your views here.
 def index(req):
-    raw = f"select ns_id, ns_content from N_summarization where ns_id = 3"
-    NC = N_summarization.objects.raw(raw)
-    # ns = NC[0].ns_content
 
     if req.method == 'POST':
         # form = TestForm(req.POST)
         form = req.POST
-        
+
         logger.info(f"index POST log test => [scroll = {form['scroll']}, deltaTime = {form['deltaTime']}]")
         # logger.info(f"index POST log test")
     else:
         logger.info("index GET log test")
 
+    query = "select * from News n inner join N_summarization_one nso on n.n_id = nso.n_id"
+    news_list = News.objects.raw(query)  # models.py Board 클래스의 모든 객체를 board_list에 담음
+    # news_list 페이징 처리
+    page = req.GET.get('page', '1')  # GET 방식으로 정보를 받아오는 데이터
+    paginator = Paginator(news_list, '10')  # Paginator(분할될 객체, 페이지 당 담길 객체수)
+    page_obj = paginator.page(page)  # 페이지 번호를 받아 해당 페이지를 리턴 get_page 권장
+
     # return render(req, "index.html", {'banner': ns})
-    return render(req, "index.html")
-
-# def i_sum(req):
-#     raw = f"select ns_id, ns_content from N_summarization where ns_id = 9"
-#     NC = N_summarization.objects.raw(raw)
-#     ns = NC[0].ns_content
-
-#     return render(req, "index.html", {'i_sum': ns})
-
-# def i_sum2(req):
-#     raw = f"select ns_id, ns_content from N_summarization where ns_id = 5"
-#     NC = N_summarization.objects.raw(raw)
-#     ns = NC[0].ns_content
-
-#     return render(req, "index.html", {'i_sum2': ns})
-
-# def i_sum3(req):
-#     raw = f"select ns_id, ns_content from N_summarization where ns_id = 7"
-#     NC = N_summarization.objects.raw(raw)
-#     ns = NC[0].ns_content
-
-#     return render(req, "index.html", {'i_sum3': ns})        
+    return render(req, "index.html", {'page_obj': page_obj, 'news_list': news_list})
 
 
 def author(req):
@@ -56,12 +40,32 @@ def author(req):
 
 
 def politics(req):
-    # post_latest = Post.objects.order_by("-createDate")[:6]
-    context = {
-        # "post_latest": post_latest
-    }
 
-    return render(req, "politics.html", context=context)
+    if req.method == 'POST':
+        # form = TestForm(req.POST)
+        form = req.POST
+
+        logger.info(f"index POST log test => [scroll = {form['scroll']}, deltaTime = {form['deltaTime']}]")
+        # logger.info(f"index POST log test")
+    else:
+        logger.info("index GET log test")
+
+    query = f"""
+        select * 
+        from News n 
+        inner join N_category_detail ncd on n.cd_id = ncd.cd_id 
+        inner join N_summarization_one nso on n.n_id = nso.n_id
+        where ncd.c_id = 100
+    """
+    news_list = News.objects.raw(query)  # models.py Board 클래스의 모든 객체를 board_list에 담음
+    # news_list 페이징 처리
+    page = req.GET.get('page', '1')  # GET 방식으로 정보를 받아오는 데이터
+    paginator = Paginator(news_list, '10')  # Paginator(분할될 객체, 페이지 당 담길 객체수)
+    page_obj = paginator.page(page)  # 페이지 번호를 받아 해당 페이지를 리턴 get_page 권장
+
+    # return render(req, "index.html", {'banner': ns})
+
+    return render(req, "politics.html", {'page_obj': page_obj, 'news_list': news_list})
 
 
 def post(req):
